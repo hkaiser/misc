b670667250e899f4df4e0203f494491fcbeaa8e6
Zach Byerly <zbyerly@phys.lsu.edu>
2014-06-02 12:27:59 -0500

    changes

--- compare_adcirc/data_level_comparison.py
+++ compare_adcirc/data_level_comparison.py
@@ -15,6 +15,15 @@ from Scientific.N import *
 from Scientific.IO.NetCDF import *
 import time
 
+def var_subtract(var_name):
+    var1 = file1.variables[var_name]
+    var2 = file2.variables[var_name]
+
+    var_output = outfile.variables[var_name]
+
+    var_output[:,:] = var1[:,:]-var2[:,:]
+
+
 if (len(sys.argv) != 4):
     print "Usage: "+str(sys.argv[0])+" infile1.nc infile2.nc outfile.nc"
     exit()
@@ -26,12 +35,11 @@ outfile_name=str(sys.argv[3])
 #Copy the first file to the outfile
 call(["nccopy",file1_name,outfile_name])
 
-
 #Read in input files
 file1 = NetCDFFile(file1_name, 'r')
 file2 = NetCDFFile(file2_name, 'r')
 
-outfile = NetCDFFile(outfile_name, 'w')
+outfile = NetCDFFile(outfile_name, 'r+')
 
 #outfile = file1 # Can I do this? No.
 
@@ -47,28 +55,30 @@ time2 = file1.variables['time']
 time1_units = time1.units
 time2_units = time2.units
 
+# Check number of time steps
+if (time1[:].size != time2[:].size):
+    print "WARNING: "+file1_name+" number of time steps does not match "+file2_name+" number of time steps."
+
+# Check time units
 if (time1_units != time2_units):
     print "WARNING: "+file1_name+" time units do not match "+file2_name+" time units."
     print time1_units
     print time2_units
 
-zeta1 = file1.variables['zeta']
-zeta2 = file2.variables['zeta']
+print "Variables: "
+for var in file1.variables.keys():
+    sys.stdout.write(var+" ")
+#    vars = file1.variables[var]
+#    print vars.getValue()
+#    vars_array = vars[:]
+#    vars_units = vars.units
+#    print vars_units
+print ""
 
-zeta_output = zeta1
 
-print file1.dimensions
+var_subtract('zeta')
 
-if (zeta1 != zeta2):
-    print "WARNING: zeta from "+file1_name+" does not match zeta from "+file1_name+"."
-    print time1
-#    for j in range(int(file1.dimensions['time'])):
-#        print j
-#        zeta_output[j,:] = zeta1[j,:]-zeta2[j,:]
-#        for i in range(file1.dimensions['node']):
-#            print zeta1[0,i]
-            
-
+# Close files            
 file1.close()
 file2.close()
 outfile.close()
