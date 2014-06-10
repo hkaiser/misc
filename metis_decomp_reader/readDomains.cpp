0752dcdb6779d77d2e343caad80b7a64fd6ade7c
zbyerly <zbyerly@durandal.cct.lsu.edu>
2014-06-10 13:46:40 -0500

    adding readdomains

--- metis_decomp_reader/readDomains.cpp
+++ metis_decomp_reader/readDomains.cpp
@@ -2,6 +2,9 @@
 #include <fstream>
 #include <stdexcept>
 #include <vector>
+#include <string>
+#include <iomanip>
+#include <sstream>
 
 class SimpleDomain
 {
@@ -28,19 +31,34 @@ public:
     
     virtual void grid()
     {
-        std::ifstream meshFile;
-        openMeshFile(meshFile);
+        std::ifstream fort80File;
         
         int numberOfDomains;
+        int numberOfElements;
+        int numberOfPoints;
+        std::vector<int> ownerID;
+
+        openfort80File(fort80File);
+        readfort80(fort80File, &numberOfDomains, &numberOfElements, &numberOfPoints, &ownerID);
+
+        for (int i=0; i< numberOfDomains; i++){
+            std::ifstream fort18File;
+            int numberOfNeighbors;
+            std::vector<int> neighborList;
+            openfort18File(fort18File, i);
+            readfort18(fort18File, &i, &numberOfNeighbors, &neighborList);            
+            std::cerr << "Domain: " << i << " number of neighbors: " << numberOfNeighbors << "\n";
+        }
         
-        readHeader(meshFile, &numberOfDomains);
+            
+
     }
 
     
 private:
     std::string meshDir;
 
-    void openMeshFile(std::ifstream& meshFile)
+    void openfort80File(std::ifstream& meshFile)
     {
         std::string meshFileName = meshDir;
         meshFileName.append("/fort.80");
@@ -50,28 +68,25 @@ private:
         }    
     }
 
-    void readHeader(std::ifstream& meshFile, int *numberOfDomains)
+    void readfort80(std::ifstream& meshFile, int *numberOfDomains, int *numberOfElements, int *numberOfPoints, std::vector<int> *ownerID)
     {
-        int numberOfElements;
-        int numberOfPoints;
-
         std::string buffer(1024, ' ');
         //Discard first 3 lines:
         std::getline(meshFile, buffer);
         std::getline(meshFile, buffer);
         std::getline(meshFile, buffer);
 
-        meshFile >> numberOfElements;
-        meshFile >> numberOfPoints;
+        meshFile >> *numberOfElements;
+        meshFile >> *numberOfPoints;
         
         //Discard rest of line
         std::getline(meshFile, buffer);
 
         meshFile >> *numberOfDomains;
 
-        std::cerr << "number of elements: " << numberOfElements << "\n"
-                  << "number of points: " << numberOfPoints << "\n"
-                  << "number of domains: " << *numberOfDomains << "\n";
+        std::cerr << "number of elements: " << *numberOfElements << "\n"
+                  << "number of points: " << *numberOfPoints << "\n"
+                  << "number of domains: " << *numberOfDomains << "\n";    
 
         //Discard rest of the line:
         std::getline(meshFile, buffer);
@@ -86,7 +101,7 @@ private:
         std::getline(meshFile, buffer);
         std::getline(meshFile, buffer);
         
-        std::cerr << buffer << "\n";
+//        std::cerr << buffer << "\n";
 
         for (int domain = 0; domain < *numberOfDomains; ++domain) {
             int buf;
@@ -99,13 +114,13 @@ private:
             //Discard rest of the line
             std::getline(meshFile, buffer);
             
-            std::cerr << "Domain: " << domain << " numNodes: " << numNodes << "\n";
+//            std::cerr << "Domain: " << domain << " numNodes: " << numNodes << "\n";
             
             for (int node = 0; node < numNodes; ++node) {
                 int nodeNum;
                 meshFile >> nodeNum;
                 
-                std::cerr << "Domain: " << domain << " Node: " << nodeNum << "\n";
+//                std::cerr << "Domain: " << domain << " Node: " << nodeNum << "\n";
             }
             
             // throw away the rest of the line
@@ -113,8 +128,7 @@ private:
         }
         // Throw away another line
         std::getline(meshFile, buffer);
-        std::vector<int> ownerID;
-        for (int node = 0; node < numberOfPoints; node++) {
+        for (int node = 0; node < *numberOfPoints; node++) {
             int global_label;
             int local_label;
             int owner;
@@ -124,14 +138,124 @@ private:
             
             std::getline(meshFile, buffer);//discard rest of line
             
-            ownerID.push_back(owner);
-        }
-
-        for (int i=0; i< numberOfPoints; i++){
-            std::cerr << i << " " << ownerID[i] << "\n";
-        }
-            
+            ownerID->push_back(owner);
+        }            
     }    
+
+    void openfort18File(std::ifstream& meshFile, int domainID)
+    {
+        std::string meshFileName = meshDir;
+        meshFileName.append("/PE");
+        std::stringstream buf;
+        buf.width(4);
+        buf.fill('0');
+        buf << domainID;
+        meshFileName.append(buf.str());
+        meshFileName.append("/fort.18");
+//        std::cerr << meshFileName << "\n";
+        meshFile.open(meshFileName.c_str());
+        if (!meshFile) {
+            throw std::runtime_error("could not open fort.80 file"+meshFileName);
+        }    
+    }
+
+
+    void readfort18(std::ifstream& meshFile, int *numberOfNeighbors, const int *domainID, std::vector<int> *neighborList)
+    {
+        int numberOfElements;
+        int numberOfNodes;
+        int domainIDFromFile;
+        int numberOfResNodes;
+        std::vector<int> residentNodes;
+
+        std::string buffer(1024, ' ');
+        //Discard first line:
+        std::getline(meshFile, buffer);
+
+        //Discard first three entries of line 2:
+        meshFile >> buffer;
+        meshFile >> buffer;
+        meshFile >> buffer;
+        
+        meshFile >> numberOfElements;
+        std::getline(meshFile, buffer); //Discard the rest of the line
+
+        for (int i=0; i<numberOfElements; i++){std::getline(meshFile, buffer);}
+
+        //Discard first three entries of next line:
+        meshFile >> buffer;
+        meshFile >> buffer;
+        meshFile >> buffer;        
+        meshFile >> numberOfNodes;
+        std::getline(meshFile, buffer); //Discard the rest of the line
+        for (int i=0; i<numberOfNodes; i++){std::getline(meshFile, buffer);}
+
+        //NFLUXF line: manually discard for now.  May need to do
+        //something more robust later.
+        std::getline(meshFile, buffer);
+
+        //Discard first three entries of next line:        
+        meshFile >> buffer;
+        meshFile >> buffer;
+        meshFile >> buffer;        
+        int NETA;
+        meshFile >> NETA;
+        std::getline(meshFile, buffer); //Discard the rest of the line
+        for (int i=0; i<NETA; i++){std::getline(meshFile, buffer);}
+
+        //Discard first three entries of next line:        
+        meshFile >> buffer;
+        meshFile >> buffer;
+        meshFile >> buffer;        
+        int NSTAE;
+        meshFile >> NSTAE;
+        std::getline(meshFile, buffer); //Discard the rest of the line
+        for (int i=0; i<NSTAE; i++){std::getline(meshFile, buffer);}
+
+        //Discard first three entries of next line:        
+        meshFile >> buffer;
+        meshFile >> buffer;
+        meshFile >> buffer;        
+        int NSTAV;
+        meshFile >> NSTAV;
+        std::getline(meshFile, buffer); //Discard the rest of the line
+        for (int i=0; i<NSTAV; i++){std::getline(meshFile, buffer);}
+        
+        //Discard NSTAM and NSTAC lines manually
+        std::getline(meshFile, buffer);        
+        std::getline(meshFile, buffer);
+        
+        //RES NODE line:
+        meshFile >> buffer;
+        meshFile >> buffer;
+        meshFile >> domainIDFromFile;
+        meshFile >> numberOfResNodes;
+        
+        for (int i=0; i<numberOfResNodes; i++)
+        {
+            int node;
+            
+            meshFile >> node;
+            residentNodes.push_back(node);
+        }        
+        std::getline(meshFile, buffer); //Discard remainder of the
+                                        //line.
+
+        meshFile >> buffer;
+        std::cerr << buffer << "\n";                
+        if (buffer=="COMM") {
+            meshFile >> buffer;
+            meshFile >> *numberOfNeighbors;
+            std::getline(meshFile, buffer);
+        }
+        
+
+//        std::getline(meshFile, buffer);
+//        std::cerr << buffer << "\n";                
+    }    
+
+
+
 };
 
     
