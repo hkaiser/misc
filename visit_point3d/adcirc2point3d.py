eabfca874cd23a21bc00459d998c49483ff7cf0e
zbyerly <zbyerly@durandal.cct.lsu.edu>
2014-06-17 14:49:41 -0500

    adcirc2point3d.py seems to work now.

--- visit_point3d/adcirc2point3d.py
+++ visit_point3d/adcirc2point3d.py
@@ -1,3 +1,44 @@
 #!/usr/bin/env python
+import math
+import sys
 
-print "hello world"
+if (len(sys.argv) != 2):
+    print "Usage: "+str(sys.argv[0])+" fort.14"
+    exit()
+
+filename=str(sys.argv[1])
+
+infile = open(filename)
+
+f = open("values.3D", "wt")
+f.write("x y z value\n");
+
+
+counter = 0
+for line in infile:
+
+#    print counter, line
+    counter=counter+1
+
+    if counter == 1:
+        continue
+    if counter == 2:
+        np = line.split()[1]
+        continue
+    
+    nodeID = line.split()[0]
+    x = float(line.split()[1])
+    y = float(line.split()[2])
+    depth = float(line.split()[3])
+    value = 0.0
+
+#    print x, y, depth
+    f.write("%g %g %g %g\n" % (x,y,depth,value))
+
+
+    if nodeID == np:
+        break
+
+
+infile.close()
+f.close()
