00c297f9a2b36d069573dfb8f5eee88455420e87
Zach Byerly <zbyerly@gmail.com>
2015-03-25 12:53:29 -0500

    fixed array sized passing via common blocks works

--- common_block_test/main.cpp
+++ common_block_test/main.cpp
@@ -1,37 +1,51 @@
 #include <iostream>
 
+#define N_MAX 20
+#define MAX_NEIGHBORS 20
+
 extern"C" {
-  void kernel_(int *n);
+  void kernel_();
+}
+
+extern"C" {
+  extern struct{
+    int n;
+    int alive[N_MAX];
+    int numneighbors[N_MAX];
+    int neighbors[MAX_NEIGHBORS][N_MAX];    
+  } variables_;
 }
 
 int main() {
 
-  int n=5;
+  int n=10;
 
-  int alive[n];
-  int numneighbors[n];
-  int neighbors[20][n];
-  
+  variables_.n = n;
+  // Creating Neighbor Connectivity
   for (int i=0; i<n; i++) {
-    alive[i] = 0;
-    numneighbors[i] = 2;
-    neighbors[0][i] = i-1;
-    neighbors[1][i] = i+1;
+    variables_.alive[i] = 0;
+    variables_.numneighbors[i] = 2;
+    // This will give the neighbor IDs in the C array index style
+    variables_.neighbors[i][0] = i-1;
+    variables_.neighbors[i][1] = i+1;
   }
-  
-  alive[0]=1;
-  
-  neighbors[0][0] = n;
-  neighbors[1][n] = 0;
 
-  kernel_(&n);
-  
-  for (int i=0; i<n; i++) {
-    std::cout << alive[i] << "\n";
+  // Creating Periodic Boundary
+  variables_.neighbors[0][0] = n-1;
+  variables_.neighbors[n-1][1] = 0;
+
+  // Seeding single alive cell
+  variables_.alive[0]=1;
+
+  //std::cout << "cpp: n=" << n << std::endl;
+
+  for (int timestep=0; timestep<10; timestep++) {
+    std::cout << "**** timestep = " << timestep << std::endl;
+    kernel_();
+    for (int i=0; i<n; i++) {
+      std::cout << "i=" << i << " alive =" << variables_.alive[i] << "\n";
+    }
   }
-  
-
-  std::cout << "Hello World!" << std::endl;
 
   return 0;
 }
