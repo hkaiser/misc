b97c420d7922fab18fc966e72d058495750f9457
Zach Byerly <zbyerly@gmail.com>
2015-05-08 17:59:17 -0500

    works for N>30, but for N<30, first two elements are zero?

--- allocate_test/kernel.f95
+++ allocate_test/kernel.f95
@@ -1,37 +1,43 @@
-SUBROUTINE INIT(N, alive_c_ptr) bind (C, name="init_")
+SUBROUTINE INIT(N, alive_c_ptr)
   use, intrinsic :: iso_c_binding
   IMPLICIT NONE
   
-  type (c_ptr), intent (out) :: alive_c_ptr
-  integer (c_int), allocatable, target, dimension (:) :: alive
-
+  type (c_ptr) :: alive_c_ptr
+  integer (c_int), allocatable, target :: alive(:)
+ 
   INTEGER :: I, N
   
-  N=10
+  N=31
   
   ALLOCATE(ALIVE(N))
 
   DO I=1,N
+     print*,"i=",i
      ALIVE(I) = I
   ENDDO
 
-  alive_c_ptr = c_loc(alive)
+  DO I=1,N
+     PRINT*, "ALIVE",I,"=",alive(I)
+  ENDDO  
+
+  alive_c_ptr = c_loc(alive(1))
 
 END SUBROUTINE INIT
 
-SUBROUTINE PRINT(N, alive_c_ptr) 
+SUBROUTINE PRINT(N, alive_c_ptr)
   use, intrinsic :: iso_c_binding
   IMPLICIT NONE
 
   INTEGER :: N,I
   type (c_ptr), intent (in) :: alive_c_ptr
-  INTEGER (c_int), DIMENSION(:), pointer :: ALIVE
+  integer (c_int), pointer :: alive(:)
   
   call c_f_pointer(alive_c_ptr, alive,[N])
 
   PRINT*, "N = ", N
-  DO I=1,N+5
-     PRINT*, "ALIVE",I,"=",ALIVE(I)
+  PRINT*, "SIZEOF(ALIVE) =", sizeof(alive)/sizeof(c_int)
+  DO I=1,N
+     PRINT*, "ALIVE",I,"=",alive(I)
   ENDDO  
 
 END SUBROUTINE PRINT
